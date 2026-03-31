e804c8382908ce43ec664f73aa595f55f978389b
Tathagata Nath <nath.riju53@gmail.com>
2026-03-31 20:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
-from langchain_huggingface import HuggingFaceEndpoint
+from langchain_community.llms import HuggingFaceHub
 
 hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
-llm = HuggingFaceEndpoint(
+llm = HuggingFaceHub(
     repo_id="google/flan-t5-base",
     huggingfacehub_api_token=hf_token,
-    temperature=0.7,
-    max_new_tokens=256
+    model_kwargs={
+        "temperature": 0.7,
+        "max_length": 256
+    }
 )
-
 st.title("Askme anything 🚀")
 
 with st.form('my_form'):
