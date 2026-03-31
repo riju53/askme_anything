72b1e291e9f7d6562a128044918c88da7cda2ca6
Tathagata Nath <nath.riju53@gmail.com>
2026-03-31 16:16:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_huggingface import HuggingFaceEndpoint
 
-# Load token securely
 hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
-# Use a PUBLIC model (important)
 llm = HuggingFaceEndpoint(
+    #repo_id="HuggingFaceH4/zephyr-7b-beta",
     repo_id="google/flan-t5-base",
     huggingfacehub_api_token=hf_token,
     temperature=0.1,
     max_new_tokens=500
 )
 
-model = ChatHuggingFace(llm=llm)
-
-# UI
 st.title("Ask me anything 🚀")
 
 with st.form('my_form'):
-    text = st.text_area('Enter text:')
+    text = st.text_area('Enter text:', '...')
     submit = st.form_submit_button('Ask')
 
 if submit:
