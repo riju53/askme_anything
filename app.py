132d54c72a9fb4db96d94d3aa39440f8df130aa0
Tathagata Nath <nath.riju53@gmail.com>
2026-04-01 20:49:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
 llm = HuggingFaceEndpoint(
     #repo_id="google/flan-t5-base",
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
     huggingfacehub_api_token=hf_token,
     temperature=0.7,
     max_new_tokens=256
